1b913c9accdd36e41abfb4a5b537f9648e16711b
Caleb Yearsley <cnyearsley@outlook.com>
2017-02-11 08:22:51 -0700

    altered buildMaze parameters

--- mazeAlgorithm.js
+++ mazeAlgorithm.js
@@ -106,7 +106,7 @@ $(function () {
 			return Math.floor(Math.random()*((b-1)-(a+1)+1)+(a+1));
 		}
 
-		function buildMaze (x1, y1, x2, y2, prevDirection) {
+		function buildMaze (x1, y1, x2, y2) {
 			if (x1 >= x2 || y1 >= y2) {
 				return;
 			}
